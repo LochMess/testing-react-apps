94e9f94684bf4c97135bb9a7037d8794b2229778
LochMess <18762221+LochMess@users.noreply.github.com>
2021-10-22 23:03:14 +1100

    Exercise 4 extra credits 1 through 3

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,34 +5,36 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
+
+const buildLoginForm = overrides => ({
+  username: faker.internet.userName(),
+  password: faker.internet.password(),
+  ...overrides,
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
   // accepts the data and assigns submittedData to the data that was submitted
   // 💰 if you need a hand, here's what the handleSubmit function should do:
-  let submittedData
-  const handleSubmit = data => (submittedData = data)
+  const handleSubmit = jest.fn()
 
   // 🐨 render the login with your handleSubmit function as the onSubmit prop
   render(<Login onSubmit={handleSubmit} />)
   // 🐨 get the username and password fields via `getByLabelText`
-  const usernameField = screen.getByLabelText(/username/i)
-  const passwordField = screen.getByLabelText(/password/i)
+  const {username, password} = buildLoginForm({username: 'example'})
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.type(screen.getByLabelText(/password/i), password)
   const submitButton = screen.getByRole('button', {name: /submit/i})
   // 🐨 use userEvent.type to change the username and password fields to
   //    whatever you want
-  const username = 'Peter Parker'
-  const password = 'let me in please?'
-  userEvent.type(usernameField, username)
-  userEvent.type(passwordField, password)
+
   // 🐨 click on the button with the text "Submit"
   userEvent.click(submitButton)
   // assert that submittedData is correct
   // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
-  expect(submittedData).toEqual({
-    username: username,
-    password: password,
-  })
+  expect(handleSubmit).toHaveBeenCalledWith({username, password})
+  expect(handleSubmit).toBeCalledTimes(1)
 })
 
 /*
