9172d0f95b27a11610a015f12ecccdf5724bb83e
LochMess <18762221+LochMess@users.noreply.github.com>
2021-10-25 19:57:11 +1100

    Exercise 7 and extra credits 1 and 2

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -8,15 +8,32 @@ import EasyButton from '../../components/easy-button'
 
 test('renders with the light styles for the light theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
-  //
+  renderWithThemeProvider(<EasyButton>Easy</EasyButton>, {theme: 'light'})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
+
   // 🐨 update the `render` call above to use the wrapper option using the
   // ThemeProvider
 })
+
+test('renders with the dark styles for the dark theme', () => {
+  renderWithThemeProvider(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
+})
+
+function renderWithThemeProvider(ui, options) {
+  const theme = options?.theme ?? 'light'
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+  )
+  return render(ui, {wrapper: Wrapper, ...options})
+}
 
 /* eslint no-unused-vars:0 */
