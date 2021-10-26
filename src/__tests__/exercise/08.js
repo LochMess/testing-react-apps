f007f90d2d5f657aaa24871997d571e13bd6943e
LochMess <18762221+LochMess@users.noreply.github.com>
2021-10-26 22:32:18 +1100

    Exercise 8 extra credits

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,7 +2,8 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, act} from '@testing-library/react'
+import {renderHook, act as hookAct} from '@testing-library/react-hooks'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
@@ -36,4 +37,53 @@ test('exposes the count and increment/decrement functions', () => {
   expect(screen.getByText(/current count/i)).toHaveTextContent('0')
 })
 
+test('extra credit 1: test with fake component', () => {
+  let result
+  function FakeComponent(props) {
+    result = useCounter(props)
+    return null
+  }
+
+  render(<FakeComponent />)
+  expect(result.count).toBe(0)
+  act(() => result.increment())
+  expect(result.count).toBe(1)
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
+})
+
+function setup(...props) {
+  const results = {}
+  function TestComponent() {
+    Object.assign(results, useCounter(...props))
+    return null
+  }
+  render(<TestComponent />)
+  return results
+}
+
+test('extra credit 2: allows customization of the initial count', () => {
+  const counterData = setup({initialCount: 13})
+  expect(counterData.count).toBe(13)
+})
+
+test('extra credit 2: allows customization of the step', () => {
+  const result = setup({step: 7})
+  expect(result.count).toBe(0)
+  act(() => result.increment())
+  act(() => result.increment())
+  expect(result.count).toBe(14)
+  act(() => result.decrement())
+  expect(result.count).toBe(7)
+})
+
+test('extra credit 3: allows customization of the step with renderHook', () => {
+  const {result} = renderHook(() => useCounter({step: 7}))
+  expect(result.current.count).toBe(0)
+  hookAct(() => result.current.increment())
+  hookAct(() => result.current.increment())
+  expect(result.current.count).toBe(14)
+  hookAct(() => result.current.decrement())
+  expect(result.current.count).toBe(7)
+})
 /* eslint no-unused-vars:0 */
